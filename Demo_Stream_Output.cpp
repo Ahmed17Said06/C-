7dfb348fe53544ff32ddb98411ea7d24d5dd814c
Ahmed Said Abdel-Sattar <97065109+Ahmed17Said06@users.noreply.github.com>
2023-03-06 21:20:45 +0200

    Add files via upload

---
+++ Demo_Stream_Output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    
+    std::cout << "Hello" << '\n';
+    std::cout << 17061997 << '\n';
+
+    int i = 0;
+    std::cout << "Enter a number: ";
+    std::cin >> i;
+    std::cout << '\n' << i << '\n';
+
+}
